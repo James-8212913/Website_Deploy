7386618bf54b109cb995ddeb64a4f01653e9a4cb
James-8212913 <>
2021-01-22 16:47:39 +1100

    Website framework

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-from flask_flatpages import FlatPages
+import markdown, pygments
+from flask import Flask, render_template, render_template_string
+from flask_flatpages import FlatPages, pygmented_markdown, pygments_style_defs
 from flask_bootstrap import Bootstrap
+from pygments import highlight
 
 
 # https://dev.to/deadwisdom/embracing-jamstack-with-python-generating-a-static-website-with-flask-and-deploying-to-netlify-4bge
@@ -10,14 +12,25 @@ from flask_bootstrap import Bootstrap
 FLATPAGES_AUTO_RELOAD = True
 FLATPAGES_EXTENSION = '.md'
 
+
+# Tell markdown to include the pygment extensions using codehilite, fenced code and tables
+def my_markdown(text):
+    markdown_text = render_template_string(text)
+    pygmented_text = markdown.markdown(markdown_text, extensions = ["codehilite", "fenced_code", "tables"])
+    return pygmented_text
+
+
 # create the opject, use this for our setting as well
 app = Flask(__name__)
-
 bootstrap = Bootstrap(app)
 
 # For settings the file itself is used
 
 app.config.from_object(__name__)
+
+# Includes the html renderer for the rendering of the pages 
+
+app.config["FLATPAGES_HTML_RENDERER"] = my_markdown
 
 # This stops Flask putting slashes after paths, becuase they get turned into 'flat files'
 
@@ -34,3 +47,7 @@ def page(path=None):
     page = pages.get_or_404(path or 'index')
      # Render the template "page.html" with our page and title
     return render_template("base.html", page=page, title=page.meta.get('title', ''))
+
+@app.route('/pygments.css')
+def pygments_css():
+    return pygments_style_defs('tango'), 200, {"Content-Type": "text/css"}

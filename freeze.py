085964233154246c68020566b40e6c9afcba830a
James-8212913 <>
2021-01-31 16:52:51 +1100

    set up

--- freeze.py
+++ freeze.py
@@ -1,10 +1,10 @@
 from flask_frozen import Freezer
 from app import app
 
+FREEZER_IGNORE_404_NOT_FOUND=True
+
 freezer = Freezer(app)
 
 if __name__ == '__main__':
     freezer.freeze()
     ##freezer.run(debug=True)
-
-FREEZER_IGNORE_404_NOT_FOUND=True

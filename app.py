9c1c4e86f55cc6766a4edd5f282544f809579d19
James-8212913 <>
2021-02-06 13:16:16 +1100

    Updated Path

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_flatpages import FlatPages, pygmented_markdown, pygments_style_defs
 from flask_bootstrap import Bootstrap
 from pygments import highlight
 
-
 # https://dev.to/deadwisdom/embracing-jamstack-with-python-generating-a-static-website-with-flask-and-deploying-to-netlify-4bge
 # This has been adopted from the tutorial above
 
@@ -12,13 +11,11 @@ from pygments import highlight
 FLATPAGES_AUTO_RELOAD = True
 FLATPAGES_EXTENSION = '.md'
 
-
 # Tell markdown to include the pygment extensions using codehilite, fenced code and tables
 def my_markdown(text):
     markdown_text = render_template_string(text)
     pygmented_text = markdown.markdown(markdown_text, extensions = ["codehilite", "fenced_code", "tables"])
     return pygmented_text
-
 
 # create the opject, use this for our setting as well
 app = Flask(__name__)
@@ -28,7 +25,7 @@ bootstrap = Bootstrap(app)
 
 app.config.from_object(__name__)
 
-# Includes the html renderer for the rendering of the pages 
+# Includes the html renderer for the rendering of the pages
 
 app.config["FLATPAGES_HTML_RENDERER"] = my_markdown
 
@@ -40,13 +37,14 @@ app.url_map.strict_slashes = False
 pages = FlatPages(app)
 
 ## Route to Flatpages and our root directory in addition to any path that ends in ".html"
-@app.route("/")
+@app.route('/')
+
 @app.route("/<path:path>.html")
 def page(path=None):
     # Look for the flatpages or find "index" if we have one loaded
-    page = pages.get_or_404(path or 'index')
+    page = pages(path or 'index')
      # Render the template "page.html" with our page and title
-    return render_template("base.html", page=page, title=page.meta.get('title', ''))
+    return render_template("base.html", page=page, title=page.meta.get('title',''))
 
 @app.route('/pygments.css')
 def pygments_css():
